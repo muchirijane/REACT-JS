a8bec1815129ef67c2541f93253e1ac077e42918
muchirijane <muchirijane@outlook.com>
2021-09-14 22:52:58 +0300

    update the post method and firebase database

--- react-learning/http-request_project/src/components/MoviesList.js
+++ react-learning/http-request_project/src/components/MoviesList.js
@@ -5,15 +5,16 @@ import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
   return (
-      <ul className={classes['movies-list']}>
-        {props.movies.map((movie) => (
-            <Movie
-                title={movie.title}
-                releaseDate={movie.releaseDate}
-                openingText={movie.openingText}
-            />
-        ))}
-      </ul>
+    <ul className={classes['movies-list']}>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
   );
 };
 
